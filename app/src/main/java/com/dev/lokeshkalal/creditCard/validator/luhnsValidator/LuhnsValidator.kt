e0d0b07bad2ed2f383381321caa292f7081849cb
Lokesh <lk.kalal@gmail.com>
2019-03-07 02:07:31 +0530

    remove is valid input

--- app/src/main/java/com/dev/lokeshkalal/creditCard/validator/luhnsValidator/LuhnsValidator.kt
+++ app/src/main/java/com/dev/lokeshkalal/creditCard/validator/luhnsValidator/LuhnsValidator.kt
@@ -1,26 +1,22 @@
 package com.dev.lokeshkalal.creditCard.validator.luhnsValidator
 
+import android.util.Log
+
 
 class LuhnsValidator : Validator {
 
 
     override fun validateCard(cardnumber: String): Boolean {
 
-        if (isValidInput(cardnumber))
+        if (cardnumber.isNullOrEmpty())
             return false
 
-        fun sumDigits(n : Int) =  n / 10 + n % 10
+        fun sumDigits(n: Int) = n / 10 + n % 10
 
-        val  reverseString = cardnumber.reversed()
+        val reverseString = cardnumber.reversed()
         val s1 = reverseString.filterIndexed { i, _ -> i % 2 == 0 }.sumBy { it - '0' }
         val s2 = reverseString.filterIndexed { i, _ -> i % 2 == 1 }.map { sumDigits((it - '0') * 2) }.sum()
         return (s1 + s2) % 10 == 0
     }
 
-    private fun isValidInput(cardNumber: String): Boolean {
-        if (cardNumber.isNullOrEmpty())
-            return false
-        cardNumber.forEach { if (it.isDigit()) return false }
-        return true
-    }
 }
